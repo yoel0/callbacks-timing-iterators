fde0cfeaf86206da3254cd58518fd5933eb26f58
yoel <yoel@macbook-pro.lan>
2020-07-16 15:36:16 -0700

    Added Calculator

--- app.js
+++ app.js
@@ -14,3 +14,48 @@ function anotherGreeting() {
 
 let myName = "Yoel";
 console.log(greeting(myName, anotherGreeting));
+
+// Excercise
+
+function printArray(array, callback) {
+  for (let i = 0; i < array.length; i++) {
+    let number = array[i];
+
+    console.log(number);
+  }
+
+  callback(array);
+}
+
+function printArrayAgain(array) {
+  for (let i = 0; i < array.length; i++) {
+    let number = array[i];
+    console.log(number);
+  }
+}
+
+const numbersArray = [1, 2, 3, 4, 5, 9, 8, 7, 6];
+
+printArray(numbersArray, printArrayAgain);
+
+// Excercise
+function calculator(num1, num2, callbackOne, callbackTwo) {
+  let result = callbackOne(num1, num2) + callbackTwo(num1, num2);
+  // 82 + -72 = 10
+
+  return result;
+}
+
+function addNumbers(num1, num2) {
+  let result = num1 + num2;
+
+  return result;
+}
+
+function subtractNumbers(num1, num2) {
+  let result = num1 - num2;
+
+  return result;
+}
+
+console.log(calculator(5, 77, addNumbers, subtractNumbers));
